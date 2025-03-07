f492cb9ac963301873c15b921ba9381255ae29ba
Leonora Sherifi <58704979+leonorasherifi@users.noreply.github.com>
2025-03-07 20:57:41 +0100

    a

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,16 +3,3 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
-
-
-
-
-
-
-
-
-
-
-  // "homepage": "https://leonorasherifi.github.io/Wheel-of-Choices",
-      // "predeploy":"npm run build",
-    // "deploy":"gh-pages -d build",
