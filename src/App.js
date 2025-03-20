2bf30c66f788500554681d1506428b5db65278e3
Leonora Sherifi <58704979+leonorasherifi@users.noreply.github.com>
2025-03-20 15:43:54 +0100

    changes on the main page

--- src/App.js
+++ src/App.js
@@ -6,27 +6,27 @@ import ColorsWheel from "./pages/ColorsWheel";
 
 const SpinningWheel = ({ title }) => {
   return (
-   <div className="flex flex-col items-center p-4 border rounded-lg shadow-lg w-80">
-  <img 
-    src={`/images/spinning-wheel.jpg`}
-    alt={`${title} Wheel`} 
-    className="w-24 h-24 object-cover mb-3"
-  />
-  {/* <h2 className="text-xl font-bold mb-2">{title}</h2> */}
-  <Link to={`/${title.toLowerCase()}`}>
-    <button className="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
-    Wheel of {title} 
-    </button>
-  </Link>
-</div>
+    <div className="wheel-card flex flex-col items-center p-4 border rounded-lg shadow-lg w-80">
+      <img 
+        src={`/images/spinning-wheel-1.png`}
+        alt={`${title} Wheel`} 
+        className="w-52 h-52 object-cover mb-3"
+      />
+      {/* <h2 className="text-xl font-bold mb-2">{title}</h2> */}
+      <Link to={`/${title.toLowerCase()}`}>
+        <button className="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+         {title} 
+        </button>
+      </Link>
+    </div>
 
   );
 };
 
 const Home = () => (
   <div className="flex flex-col items-center gap-8 p-10">
-  <h1 className="text-3xl font-bold text-center">Spinning Wheel App</h1>
-  <div className="flex flex-wrap justify-center gap-6 w-full max-w-4xl">
+  <h1 className="wheel-title text-5xl font-bold text-center">Wheel Of Choices</h1>
+  <div className="cards-section flex flex-wrap justify-center gap-6 w-full">
     <SpinningWheel title="Names" />
     <SpinningWheel title="Numbers" />
     <SpinningWheel title="Colors" />
